d4f16a67830dbdcc60107d5eae1dd908c1fec19c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-16 15:34:44 +0200

    swift 2

--- Swift01.playground/Contents.swift
+++ Swift01.playground/Contents.swift
@@ -7,3 +7,12 @@ var welcomeMessage = "Hello!"
 welcomeMessage = "Hello World!"
 
 print(welcomeMessage)
+
+// It is Comments
+
+/*
+ let maximumNumberAttemps = 10
+ var currentAttemps = 1
+ */
+
+let dog = "John"; print(dog)
