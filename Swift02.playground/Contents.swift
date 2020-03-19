f3e8462af0e1a91408fc7e3572ef167aea5257ae
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-19 21:06:48 +0200

    unicode

--- Swift02.playground/Contents.swift
+++ Swift02.playground/Contents.swift
@@ -84,3 +84,19 @@ for scene in scennesPiece {
     }
 }
 print("Total \(actOneSceneCount)")
+
+let dogString = "Dog℉😜"
+dogString.count
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit)", terminator: " ")
+}
+print()
+
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit)", terminator: " ")
+}
+print()
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value)", terminator: " ")
+}
+print()
