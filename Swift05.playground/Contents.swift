73782dab106340c2a60ab216cf45fa57f41c83a7
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-20 20:22:10 +0200

    early exit

--- Swift05.playground/Contents.swift
+++ Swift05.playground/Contents.swift
@@ -85,3 +85,17 @@ gameLoop: while square != finalSquare {
 }
 print("Game over")
 
+func greet(person: [String: String]) {
+    guard let name = person["name"] else {
+        return
+    }
+    print("Hello, \(name)")
+    
+    guard let location = person["location"] else {
+        print("I hope you are right")
+        return
+    }
+    print("I hope in \(location) good weather")
+}
+greet(person: ["name": "John"])
+greet(person: ["name": "Jane", "location": "Cupertino"])
