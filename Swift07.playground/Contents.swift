1226368e44e59b7908e0b9e504fdb5c9cd300d6c
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 18:12:13 +0200

    getter setter

--- Swift07.playground/Contents.swift
+++ Swift07.playground/Contents.swift
@@ -117,3 +117,62 @@ let alsoTenEighty = tenEighty
 alsoTenEighty.frameRate = 30.0
 
 print("Frame rate: \(tenEighty.frameRate)")
+
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+rangeOfThreeItems.firstValue = 6
+
+class DataImporter {
+    var fileName = "data.txt"
+}
+class DataManager {
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+let manager = DataManager()
+manager.data.append("Something")
+manager.data.append("Something more")
+print("\(manager.importer.fileName)")
+
+struct Point {
+    var x = 0.0
+    var y = 0.0
+}
+struct Size {
+    var width = 0.0
+    var height = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + size.width / 2
+            let centerY = origin.y + size.height / 2
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter) {
+            origin.x = newCenter.x - size.width / 2
+            origin.y = newCenter.y - size.height / 2
+        }
+    }
+}
+var square = Rect(origin: Point(x: 0.0, y: 0.0), size: Size(width: 10.0, height: 8.0))
+let initialSquareCenter = square.center
+square.center = Point(x: 7.0, y: 6.0)
+print(square.origin)
+
+struct Cuboid {
+    var width = 0.0
+    var height = 0.0
+    var depth = 0.0
+    var volume: Double {
+        return width * height * depth
+    }
+}
+let cub = Cuboid(width: 4, height: 5, depth: 2)
+print(cub.volume)
