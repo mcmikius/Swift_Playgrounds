93fae5691c7e42e4589e641b295bc4469434df45
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 14:12:15 +0200

    closure

--- Swift06.playground/Contents.swift
+++ Swift06.playground/Contents.swift
@@ -130,3 +130,31 @@ func serve(customer customerProvider: @autoclosure () -> String) {
 }
 serve(customer: customersInLine.remove(at: 0))
 print(customersInLine.count)
+
+var names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by: backward)
+reversedNames = names.sorted(by: { (s1: String, s2: String) -> Bool in
+    return s1 > s2
+})
+reversedNames = names.sorted(by: { s1, s2 in
+    return s1 > s2
+})
+reversedNames = names.sorted(by: { $0 > $1 })
+reversedNames = names.sorted(by: > )
+print(reversedNames)
+
+let digitNames = [0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine"]
+let numbers = [18, 63, 345]
+let strings = numbers.map { (number) -> String in
+    var number = number
+    var output = ""
+    repeat {
+        output = digitNames[number % 10]! + output
+        number /= 10
+    } while number > 0
+    return output
+}
+print(strings)
