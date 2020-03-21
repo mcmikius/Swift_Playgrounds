01130294700c2c7e30cb438f667218423f4620a7
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 13:39:00 +0200

    func visibility

--- Swift06.playground/Contents.swift
+++ Swift06.playground/Contents.swift
@@ -67,3 +67,22 @@ print(instance.x)
 
 completionHandlers.first?()
 print(instance.x)
+
+func chooseStepFunction(backward: Bool) -> (Int) -> Int {
+    func stepForward(_ input: Int) -> Int {
+        return input + 1
+    }
+    func stepBackward(_ input: Int) -> Int {
+        return input - 1
+    }
+    return backward ? stepBackward : stepForward
+}
+
+var currentValue = -4
+let moveNearerToZero = chooseStepFunction(backward: currentValue > 0)
+print("Counting to zero...")
+while currentValue != 0 {
+    print("\(currentValue)...")
+    currentValue = moveNearerToZero(currentValue)
+}
+print("ZERO!")
