1df20e7ff82565810f0aba9eaca99335549d0822
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 22:16:29 +0200

    inherence

--- Swift07.playground/Contents.swift
+++ Swift07.playground/Contents.swift
@@ -409,3 +409,34 @@ matrix[0, 1] = 1.5
 matrix[1, 0] = 3.2
 
 let someValue = matrix[0, 1]
+
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "move with speed \(currentSpeed) km per h"
+    }
+    func makeNoise() {
+        
+    }
+}
+let someVehicle = Vehicle()
+print("Vehicle: \(someVehicle.description)")
+
+class Bicycle: Vehicle {
+    var hasBasket = false
+}
+
+let bicycle = Bicycle()
+bicycle.hasBasket = true
+bicycle.currentSpeed = 15.0
+print("Bicycle: \(bicycle.description)")
+
+class Tandem: Bicycle {
+    var currentNumberOfPassengers = 0
+}
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentNumberOfPassengers = 2
+tandem.currentSpeed = 22.0
+print("Tandem: \(tandem.description)")
+
