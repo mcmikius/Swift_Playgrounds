edb34e90e87a9de58c45f7888b7fc6a1dd15b1ae
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 19:57:57 +0200

    subscript

--- Swift07.playground/Contents.swift
+++ Swift07.playground/Contents.swift
@@ -370,3 +370,13 @@ if player.tracker.advance(to: 2) {
     print("Level 6 is not ")
 }
 
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+let threeTimesTable = TimesTable(multiplier: 3)
+print("6 * 3 = \(threeTimesTable[6])")
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
