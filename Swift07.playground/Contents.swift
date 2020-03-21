4b0d7ea55b46aa3ef690839fd94f90213a51a152
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 15:16:40 +0200

    enum

--- Swift07.playground/Contents.swift
+++ Swift07.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
 
-var str = "Hello, playground"
+enum CompassPoint {
+    case north
+    case south
+    case east
+    case west
+}
+
+enum Planet {
+    case mercury, venus, earth, mars, jupiter, saturn, uranus, neptune
+}
+
+var directionToHead = CompassPoint.west
+directionToHead = .east
+
+switch directionToHead {
+case .north:
+    print("Let go to North")
+case .south:
+    print("Let go to South")
+case .east:
+    print("Let go to East")
+case .west:
+    print("Let go to West")
+}
+
+let somePlanet = Planet.earth
+switch somePlanet {
+case .earth:
+    print("It has Life")
+default:
+    print("It has not life")
+}
