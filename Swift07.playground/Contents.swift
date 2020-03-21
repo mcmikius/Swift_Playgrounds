bb92c07839c833e4d8e1d1084fec6092c2c04e14
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-21 20:08:19 +0200

    subscripts

--- Swift07.playground/Contents.swift
+++ Swift07.playground/Contents.swift
@@ -380,3 +380,32 @@ let threeTimesTable = TimesTable(multiplier: 3)
 print("6 * 3 = \(threeTimesTable[6])")
 var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
 numberOfLegs["bird"] = 2
+
+struct Matrix {
+    let rows: Int
+    let colums: Int
+    var grid: [Double]
+    init(rows: Int, colums: Int) {
+        self.rows = rows
+        self.colums = colums
+        grid = Array(repeating: 0.0, count: rows * colums)
+    }
+    func indexIsValid(row: Int, colum: Int) -> Bool {
+        return row >= 0 && row < rows && colum >= 0 && colum < colums
+    }
+    subscript(row: Int, colum: Int) -> Double {
+        get {
+            assert(indexIsValid(row: row, colum: colum), "Index out of range")
+            return grid[row * colums + colum]
+        }
+        set {
+            assert(indexIsValid(row: row, colum: colum), "Index out of range")
+            grid[row * colums + colum] = newValue
+        }
+    }
+}
+var matrix = Matrix(rows: 2, colums: 2)
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+let someValue = matrix[0, 1]
