cc1c5ff4937d4a1ee61a5f339f8a760e865b2983
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-22 12:05:41 +0200

    init

---
+++ Swift08.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+struct Fahrenheit {
+    var temperature = 32.0
+}
+var f = Fahrenheit()
+print("Fahrenheit temperature value \(f.temperature)")
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 8.0
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+    init(_ celsius: Double) {
+        temperatureInCelsius = celsius
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+boilingPointOfWater.temperatureInCelsius
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+freezingPointOfWater.temperatureInCelsius
+let bodyTemperature = Celsius(36.6)
+bodyTemperature.temperatureInCelsius
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+    }
+    init(white: Double) {
+        red = white
+        green = white
+        blue = white
+    }
+}
+
+let magenta = Color(red: 1.0, green: 0.0, blue: 1.0)
+let halfGrey = Color(white: 0.5)
+
