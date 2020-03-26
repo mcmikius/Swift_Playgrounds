da602d4e6325366d2925fb60bd2b618116ff9530
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-26 21:26:07 +0200

    strong reference

--- Swift10.playground/Contents.swift
+++ Swift10.playground/Contents.swift
@@ -23,3 +23,35 @@ referenceOne = nil
 referenceTwo = nil
 referenceThree = nil
 
+class PersonTwo {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    var apartment: Apartment?
+    deinit {
+        print("\(name) deinit")
+    }
+}
+
+class Apartment {
+    let unit: String
+    init(unit: String) {
+        self.unit = unit
+    }
+    var tenant: PersonTwo?
+    deinit {
+        print("\(unit) deinit")
+    }
+}
+
+var john: PersonTwo?
+var unit4a: Apartment?
+john = PersonTwo(name: "John Appleseed")
+unit4a = Apartment(unit: "4a")
+john!.apartment = unit4a
+unit4a!.tenant = john
+
+john = nil
+unit4a = nil
+
