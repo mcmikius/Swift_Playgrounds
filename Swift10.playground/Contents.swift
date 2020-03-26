bf383d3c1c66acfe990821c11b7308e36a363f23
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-26 22:29:56 +0200

    optional chain

--- Swift10.playground/Contents.swift
+++ Swift10.playground/Contents.swift
@@ -82,3 +82,76 @@ var johnApple: Customer?
 johnApple = Customer(name: "John Appleseed")
 johnApple?.card = CreditCard(number: 1234567812345678, customer: johnApple!)
 johnApple = nil
+
+class Country {
+    let name: String
+    var capitalCity: City!
+    init(name: String, capitalCity: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalCity, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+
+var country = Country(name: "Ukraine", capitalCity: "Kyiv")
+print("Capital city of \(country.name) is \(country.capitalCity.name)")
+
+class HTMLElement {
+    let name: String
+    let text: String?
+    
+    lazy var asHTML: () -> String = { [unowned self] in
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "</\(self.name)>"
+        }
+    }
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        print("\(name) deinit")
+    }
+}
+
+let heading = HTMLElement(name: "h1")
+let defaultText = "some default text"
+heading.asHTML = {
+    return "<\(heading.name)>\(heading.text ?? defaultText)</\(heading.name)>"
+}
+print(heading.asHTML())
+
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "Hello World!")
+print(paragraph!.asHTML())
+paragraph = nil
+
+
+class PersonThree {
+    var residence: Residence?
+}
+class Residence {
+    var numberOfRooms = 1
+}
+let johnTwo = PersonThree()
+if let roomCount = johnTwo.residence?.numberOfRooms {
+    print("\(roomCount)")
+} else {
+    print("nil")
+}
+johnTwo.residence = Residence()
+if let roomCount = johnTwo.residence?.numberOfRooms {
+    print("\(roomCount)")
+} else {
+    print("nil")
+}
