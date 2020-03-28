01a38cfc1eaa7e5c6aeadaac22a0db899eb3f191
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-03-28 21:17:41 +0200

    throws

---
+++ Swift11.playground/Contents.swift
@@ -0,0 +1,51 @@
+import UIKit
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case insufficientFunds(coinsNeeded: Int)
+    case outOfStock
+}
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachine {
+    var inventery = [
+        "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    var coinDeposited = 0
+    func vend(itemNamed name: String) throws {
+        guard let item = inventery[name] else {
+            throw VendingMachineError.invalidSelection
+        }
+        guard item.count > 0 else {
+            throw VendingMachineError.outOfStock
+        }
+        guard item.price <= coinDeposited else {
+            throw VendingMachineError.insufficientFunds(coinsNeeded: item.price - coinDeposited)
+        }
+        
+        coinDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventery[name] = newItem
+        
+        print("Sale: \(name)")
+    }
+}
+
+let favoriteSnacks = [
+    "Alice": "Chips",
+    "Bob": "Licorice",
+    "Eve": "Pretzels"
+]
+
+func buyFavoriteSnack(person: String, vendingMachine: VendingMachine) throws {
+    let snackName = favoriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackName)
+}
