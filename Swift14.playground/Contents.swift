5a6bc4c13f3dfb18c59cbd08cc92a2defe3ddb0f
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-04-12 21:02:42 +0300

    where

--- Swift14.playground/Contents.swift
+++ Swift14.playground/Contents.swift
@@ -40,9 +40,12 @@ protocol Container {
     mutating func append(_ item: Item)
     var count: Int { get }
     subscript(i: Int) -> Item { get }
+    associatedtype Iterator: IteratorProtocol where Iterator.Element == Item
+    func makeIterator() -> Iterator
 }
 
 struct IntStack: Container {
+    
     mutating func append(_ item: Int) {
         self.push(item)
     }
@@ -63,6 +66,9 @@ struct IntStack: Container {
     }
     mutating func pop() -> Int {
         return items.removeLast()
+    }
+    func makeIterator() -> Array<Int>.Iterator {
+        return Array<Int>.Iterator(_elements: items)
     }
 }
 
@@ -86,7 +92,11 @@ struct Stack<Element>: Container {
     subscript(i: Int) -> Element {
         return items[i]
     }
+    func makeIterator() -> Array<Element>.Iterator {
+        return Array<Element>.Iterator(_elements: items)
+    }
 }
+
 
 extension Array: Container {}
 
@@ -146,3 +156,62 @@ if allItemsMatch(stackOfString, arrayOfString) {
 } else {
     print("not match")
 }
+
+extension Stack where Element: Equatable {
+    func isTop(_ item: Element) -> Bool {
+        guard let topItem = items.last else {
+            return false
+        }
+        return topItem == item
+    }
+}
+
+if stackOfString.isTop("tres") {
+    print("Top element tres")
+} else {
+    print("Something else")
+}
+
+struct NotEquatable {}
+
+var notEquatableStack = Stack<NotEquatable>()
+let notEquatableValue = NotEquatable()
+notEquatableStack.push(notEquatableValue)
+//notEquatableStack.isTop(notEquatableValue)
+extension Container where Item: Equatable {
+    func startsWith(_ item: Item) -> Bool {
+        return count >= 1 && self[0] == item
+    }
+}
+if [9, 9, 9].startsWith(42) {
+    print("Start from 42")
+} else {
+    print("Start from something else")
+}
+
+extension Container where Item == Double {
+    func averange() -> Double {
+        var sum = 0.0
+        for index in 0..<count {
+            sum += self[index]
+        }
+        return sum / Double(count)
+    }
+}
+
+print([1260.0, 1200.0, 98.6, 37.0].averange())
+
+protocol ComparableContainer: Container where Item: Comparable {}
+
+extension Container {
+    subscript<Indices: Sequence>(indices: Indices) -> [Item]
+        where Indices.Iterator.Element == Int {
+            var result = [Item]()
+            for index in indices {
+                result.append(self[index])
+            }
+            return result
+            
+    }
+}
+
