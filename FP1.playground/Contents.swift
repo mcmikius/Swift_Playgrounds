3177f6c004c991a09995fca8183da155e765c8fe
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-04-19 19:43:47 +0300

    recursion done

--- FP1.playground/Contents.swift
+++ FP1.playground/Contents.swift
@@ -1,27 +1,31 @@
-func choosePlusMinus(isPlus: Bool) -> (Int, Int) -> Int {
-    func plus(a: Int, b: Int) -> Int {
-        return a + b
-    }
-    func minus(a: Int, b: Int) -> Int {
-        return a - b
-    }
-    return isPlus ? plus : minus
-}
+import Foundation
 
-func extractFullName(firstName: String, lastName: String) -> String {
-    return "\(firstName) \(lastName)"
-}
-func explicityRetunClosure(firstName: String) -> (String) -> String {
-    return {
-        (lastName: String) -> String in
-        return "\(firstName) \(lastName)"
-    }
-}
-let fnIncludingFirstName = explicityRetunClosure(firstName: "John")
-let extractedFullName = fnIncludingFirstName("Doe")
+//func choosePlusMinus(isPlus: Bool) -> (Int, Int) -> Int {
+//    func plus(a: Int, b: Int) -> Int {
+//        return a + b
+//    }
+//    func minus(a: Int, b: Int) -> Int {
+//        return a - b
+//    }
+//    return isPlus ? plus : minus
+//}
+//
+//func extractFullName(firstName: String, lastName: String) -> String {
+//    return "\(firstName) \(lastName)"
+//}
+//func explicityRetunClosure(firstName: String) -> (String) -> String {
+//    return {
+//        (lastName: String) -> String in
+//        return "\(firstName) \(lastName)"
+//    }
+//}
+//let fnIncludingFirstName = explicityRetunClosure(firstName: "John")
+//let extractedFullName = fnIncludingFirstName("Doe")
 
 func factorial(n: Int) -> Int {
-    return n * factorial(n: n - 1)
+    return n == 0 || n == 1 ? 1 : n * factorial(n: n - 1)
 }
 
 print(factorial(n: 5))
+
+
