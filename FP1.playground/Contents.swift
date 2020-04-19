53cf74fc75441441eb48c5e5e346102508747c0f
Mykhailo Bondarenko <mcmikius@icloud.com>
2020-04-19 19:51:03 +0300

    recursion

--- FP1.playground/Contents.swift
+++ FP1.playground/Contents.swift
@@ -28,4 +28,24 @@ func factorial(n: Int) -> Int {
 
 print(factorial(n: 5))
 
+func powerOfTwo(n: Int) -> Int {
+    return n == 0 ? 1 : 2 * powerOfTwo(n: n - 1)
+}
 
+let fnResult = powerOfTwo(n: 3)
+
+func power2(n: Int) -> Int {
+    var y = 1
+    for _ in 0...n - 1 {
+        y *= 2
+    }
+    return y
+}
+
+let result = power2(n: 4)
+
+func repateString(str: String, n: Int) -> String {
+    return n == 0 ? "" : str + repateString(str: str , n: n - 1)
+}
+
+print(repateString(str: "Hello", n: 4))
